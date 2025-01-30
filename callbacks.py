0805f43a29eb559b08a5e22a89220a1d6144e61f
Prajjwal Mehta <prajj1999@gmail.com>
2025-01-30 01:41:33 -0500

    Fix callback step

--- callbacks.py
+++ callbacks.py
@@ -1,6 +1,7 @@
 from stable_baselines3.common.callbacks import BaseCallback
 import numpy as np
 import wandb
+from typing import List, Dict
 
 
 class CurriculumCallback(BaseCallback):
@@ -10,10 +11,11 @@ class CurriculumCallback(BaseCallback):
         self.difficulty_threshold = difficulty_threshold
         self.check_freq = check_freq
         self.n_calls = 0
+        self.ep_info_buffer = []
 
     def _init_callback(self) -> None:
         """Initialize callback-specific variables."""
-        pass
+        self.ep_info_buffer = []
 
     def _on_step(self) -> bool:
         """
@@ -21,14 +23,17 @@ class CurriculumCallback(BaseCallback):
         """
         self.n_calls += 1
 
+        # Get episode info
+        if len(self.training_env.get_attr('rewards')[0]) > 0:
+            self.ep_info_buffer.append({
+                'is_success': self.training_env.get_attr('rewards')[0][-1] > 0
+            })
+
         if self.n_calls % self.check_freq == 0:
-            # Get episode info from the last few episodes
-            if len(self.model.ep_info_buffer) > 0:
-                ep_info = self.model.ep_info_buffer[-1]
-                success_rate = np.mean([info['is_success']
-                                        for info in self.model.ep_info_buffer[-10:]
-                                        if 'is_success' in info
-                                        ])
+            # Calculate success rate from recent episodes
+            if len(self.ep_info_buffer) > 10:
+                recent_episodes = self.ep_info_buffer[-10:]
+                success_rate = np.mean([info['is_success'] for info in recent_episodes])
 
                 if success_rate > self.difficulty_threshold:
                     current_difficulty = self.eval_env.get_attr('difficulty')[0]
@@ -44,5 +49,8 @@ class CurriculumCallback(BaseCallback):
                         "curriculum/difficulty": new_difficulty,
                         "curriculum/success_rate": success_rate
                     })
+
+                # Clear buffer after checking
+                self.ep_info_buffer = []
 
         return True
